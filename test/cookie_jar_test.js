346e9f837c7104a2e59235151955dcdc5f6eaeb5
Doron Izgalov <doroni@jfrog.com>
2024-05-16 00:19:37 +0300

    Patch 2.5.0 test

--- test/cookie_jar_test.js
+++ test/cookie_jar_test.js
@@ -479,8 +479,31 @@ vows
         assert.strictEqual(cookies[0].value, 'FooBar');
       }
     }
-  })
-  .addBatch({
+  }).addBatch({
+  "Issue #282 - Prototype pollution": {
+    "when setting a cookie with the domain __proto__": {
+      topic: function() {
+        const jar = new tough.CookieJar(undefined, {
+          rejectPublicSuffixes: false
+        });
+        // try to pollute the prototype
+        jar.setCookieSync(
+          "Slonser=polluted; Domain=__proto__; Path=/notauth",
+          "https://__proto__/admin"
+        );
+        jar.setCookieSync(
+          "Auth=Lol; Domain=google.com; Path=/notauth",
+          "https://google.com/"
+        );
+        this.callback();
+      },
+      "results in a cookie that is not affected by the attempted prototype pollution": function() {
+        const pollutedObject = {};
+        assert(pollutedObject["/notauth"] === undefined);
+      }
+    }
+  }
+  }).addBatch({
     "Remove all cookies": {
       topic: function () {
         var jar = new CookieJar();
